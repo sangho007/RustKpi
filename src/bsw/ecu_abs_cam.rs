5e10983c1da70fad38dd340022ab1d5d668fde22
sangho007 <lk4820asdf@gmail.com>
2025-10-19 02:14:28 +0900

    FFI add

--- src/bsw/ecu_abs_cam.rs
+++ src/bsw/ecu_abs_cam.rs
@@ -14,7 +14,7 @@ pub async fn ea_cam_provider(camera: CameraChannels) -> Result<()> {
         let mut alive_cnt = 0;
         
         // 비디오 파일이나 카메라 장치를 엽니다.
-        let cammode = true;
+        let cammode = false;
         let (mut cap, frame_width, frame_height): (Box<dyn cam_lib::FrameCapture>, u32, u32) = if cammode {
             let libcam = cam_lib::libcamera_capture::LibcameraCapture::new(1280, 720, 30)
                 .map_err(|e| opencv::Error::new(
