d14c0641d79eebcb89afe27404542e838fa28d56
sangho007 <lk4820asdf@gmail.com>
2025-11-05 17:01:51 +0900

    ..

--- src/calibration/adas_cod.rs
+++ src/calibration/adas_cod.rs
@@ -72,8 +72,8 @@ impl Default for AdasLongitudinalCalibration {
     fn default() -> Self {
         Self {
             control_period: Duration::from_millis(50),
-            cruise_speed_percent: 60,
-            crawl_speed_percent: 25,
+            cruise_speed_percent: 30,
+            crawl_speed_percent: 20,
             slowdown_distance_cm: 60.0,
             stop_distance_cm: 35.0,
             log_interval: Duration::from_secs(1),
