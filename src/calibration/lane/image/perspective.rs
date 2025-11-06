a316df2ddeb44a248dfbea96b5f6b614df4af16c
sangho007 <lk4820asdf@gmail.com>
2025-11-06 15:40:46 +0900

    1차 파라미터 수정~

--- src/calibration/lane/image/perspective.rs
+++ src/calibration/lane/image/perspective.rs
@@ -40,7 +40,7 @@ impl Default for PerspectiveCalibration {
         Self {
             source: [
                 (0.0, 240.0),
-                (160.0, 150.0),
+                (170.0, 150.0),
                 (480.0, 150.0),
                 (640.0, 240.0),
             ],
--- src/calibration/lane/image/roi.rs
+++ src/calibration/lane/image/roi.rs
@@ -28,7 +28,7 @@ impl Default for RoiCalibration {
         Self {
             vertices:[
             (0, 240),
-            (160, 100),
+            (170, 100),
             (480, 100),
             (640, 240),
             ],
