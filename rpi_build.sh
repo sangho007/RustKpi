e6f858c53991cd42c60fc4221c45e51879268d52
sangho007 <lk4820asdf@gmail.com>
2025-11-07 01:21:12 +0000

    ..

--- rpi_build.sh
+++ rpi_build.sh
@@ -1,1 +1,9 @@
+cd /rust_ws/RustKpi
+
 cargo build --release && cp ./target/aarch64-unknown-linux-gnu/release/RustKpi .
+
+git add .
+git commit -m ".."
+git push origin master
+
+echo task done!!!
--- src/asw/adas_cod.rs
+++ src/asw/adas_cod.rs
@@ -139,9 +139,12 @@ pub async fn runnable_adas_lateral(id: &'static str, channels: RteChannels) {
             .unwrap_or(0.0);
 
         //let base_cmd = calib.servo_neutral_deg as f64 - pid_output; -> good !!!!!
-        println!("[ADAS-COD] current Lat error : {}", current_error.unwrap_or(0.0));
+
         let base_cmd = calib.servo_neutral_deg as f64
             - 100.0 * current_error.unwrap_or(0.0);
+        println!("[ADAS-COD] current Lat error : {}", current_error.unwrap_or(0.0));
+        println!("[ADAS-COD] base cmd : {}", base_cmd);
+
             //+ 0.05 * lane_offset_px;
 
         let target_deg = base_cmd
