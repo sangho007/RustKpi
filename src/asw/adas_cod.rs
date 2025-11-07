2811ebb39a4d599551ebdc7a0b5bbecdbba9fb44
sangho007 <lk4820asdf@gmail.com>
2025-11-07 12:23:08 +0900

    ..

--- src/asw/adas_cod.rs
+++ src/asw/adas_cod.rs
@@ -118,7 +118,7 @@ pub async fn runnable_adas_lateral(id: &'static str, channels: RteChannels) {
 
 
         let base_cmd = calib.servo_neutral_deg as f64
-            - 150.0 * current_error.unwrap_or(0.0);
+            - 450.0 * current_error.unwrap_or(0.0);
 
             //+ 0.05 * lane_offset_px;
 
