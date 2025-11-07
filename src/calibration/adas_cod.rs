0721d5c7eb5143c672a22909274d0468d88e0ec9
sangho007 <lk4820asdf@gmail.com>
2025-11-07 16:32:53 +0900

    ..

--- src/calibration/adas_cod.rs
+++ src/calibration/adas_cod.rs
@@ -131,7 +131,7 @@ impl Default for AdasLongitudinalCalibration {
             cruise_speed_percent: 30,
             crawl_speed_percent: 20,
             speed_target_mps: 0.35,
-            speed_pid_kp: 30.0,
+            speed_pid_kp: 50.0,
             speed_pid_ki: 0.5,
             speed_pid_kd: 8.0,
             speed_pid_integral_limit: 0.4,
