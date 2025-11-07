5661d0dd6b8f58ad63415912ff96ac81640f12e3
sangho007 <lk4820asdf@gmail.com>
2025-11-07 10:00:38 +0900

    1차 파라미터 수정~

--- src/asw/adas_cod.rs
+++ src/asw/adas_cod.rs
@@ -139,8 +139,8 @@ pub async fn runnable_adas_lateral(id: &'static str, channels: RteChannels) {
 
         //let base_cmd = calib.servo_neutral_deg as f64 - pid_output; -> good !!!!!
         let base_cmd = calib.servo_neutral_deg as f64
-            - 10.0 * current_error.unwrap_or(0.0)
-            + 0.05 * lane_offset_px;
+            - 10.0 * current_error.unwrap_or(0.0);
+            //+ 0.05 * lane_offset_px;
 
         let target_deg = base_cmd
             .round()
