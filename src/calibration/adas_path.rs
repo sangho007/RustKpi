e3305c985c11457287424e2110306ee820c58dd9
sangho007 <lk4820asdf@gmail.com>
2025-11-10 15:20:12 +0900

    ..신호등

--- src/calibration/adas_path.rs
+++ src/calibration/adas_path.rs
@@ -85,7 +85,7 @@ impl Default for AdasPathGlobalCalibration {
             max_lane_change_candidates: 8,
             nearest_search_horizon: 12,
             nearest_heading_cos_threshold: 0.0,
-            lane_change_retry_cooldown: Duration::from_millis(500),
+            lane_change_retry_cooldown: Duration::from_millis(5000),
             obstacle_lane_change_cooldown: Duration::from_millis(1000),
             obstacle_block_margin_m: 1.0,
             obstacle_block_timeout: Duration::from_millis(5000),
