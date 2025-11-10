6b150bc09c22aefe76c6238fc384c92a8aa9fa71
sangho007 <lk4820asdf@gmail.com>
2025-11-10 14:41:30 +0900

    ..신호등

--- src/calibration/forward_collision.rs
+++ src/calibration/forward_collision.rs
@@ -13,7 +13,7 @@ impl Default for ForwardCollisionCalibration {
     fn default() -> Self {
         Self {
             stop_request_distance_cm: 20.0,
-            lane_change_request_distance_cm: 75.0,
+            lane_change_request_distance_cm: 105.0,
         }
     }
 }
