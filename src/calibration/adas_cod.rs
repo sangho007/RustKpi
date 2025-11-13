7a16542daa217e4b0bafa3782add2d5e46951060
sangho007 <lk4820asdf@gmail.com>
2025-11-13 11:01:45 +0900

    .. 주행 상태머신 수정

--- src/calibration/adas_cod.rs
+++ src/calibration/adas_cod.rs
@@ -47,7 +47,7 @@ impl AdasLateralCalibration {
             w_lane_angle: 0.0,
             pid_kp: -1000.0,
             pid_ki: 0.0,
-            pid_kd: 10.0,
+            pid_kd: 5.0,
             pid_integral_limit: 0.5,
             pid_sample_index: 6,
             servo_neutral_deg: 90,
